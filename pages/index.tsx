099480e21c3d8bfad1240e95702c2ebbc3347ede
Adam-pw <adampithewan@gmail.com>
2023-06-16 23:22:12 +0530

    feat: :bug: fixed all the bugs final

--- pages/index.tsx
+++ pages/index.tsx
@@ -172,7 +172,7 @@ export default function Home() {
             <div role="status">
               <svg
                 aria-hidden="true"
-                className="w-16 h-16 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                className="w-16 h-16 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-[#310036]"
                 viewBox="0 0 100 101"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
